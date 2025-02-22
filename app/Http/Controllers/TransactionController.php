bccca353589f5ef4c919630f43f1c45b43aa64b6
dedy.kusworo <dedy.kusworo@gmail.com>
2025-02-22 17:31:08 +0700

    update product

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class TransactionController extends Controller
+{
+
+    public function index()
+    {
+        // $filter=["start"=>(int)request()->start,"length"=>(int)request()->length,"draw"=>(int)request()->draw,];
+        // $payload=["id"=>0,"clientName"=>"","filter"=>$filter,];
+        // $response = Http::withBasicAuth('joe','secret')->post('http://localhost:10010/client/gets', $payload)->json();
+        // if (!is_array($response) || !isset($response['result']) || !is_array($response['result'])) {return response()->json(['error' => 'Invalid API response format or data type'], 500);}
+        // $clients = $response['result']['data'];
+        // $filter=["start"=>(int)request()->start,"length"=>(int)request()->length,"draw"=>(int)request()->draw,];
+        // $payload=[    "id"=>0,    "clientName"=>"",    "filter"=>$filter,];
+        // $response = Http::withBasicAuth('joe','secret')->post('http://localhost:10010/group/gets', $payload)->json();
+        // if (!is_array($response) || !isset($response['result']) || !is_array($response['result'])) {return response()->json(['error' => 'Invalid API response format or data type'], 500);}
+        // $groups = $response['result']['data'];
+        // $filter=["start"=>(int)request()->start,"length"=>(int)request()->length,"draw"=>(int)request()->draw,];
+        // $payload=["id"=>0,"clientName"=>"","filter"=>$filter];
+        // $response = Http::withBasicAuth('joe','secret')->post('http://localhost:10010/merchant/gets', $payload)->json();
+        // if (!is_array($response) || !isset($response['result']) || !is_array($response['result'])) {return response()->json(['error' => 'Invalid API response format or data type'], 500);}
+        // $merchants = $response['result']['data'];
+        return view('dashboard.transaction.index');
+    }
+
+
+    public function getAll()
+    {
+        $filter=[
+            "start"=>(int)request()->start,
+            "length"=>(int)request()->length,
+            "draw"=>(int)request()->draw,
+        ];
+        $payload=[
+            "id"=>0,
+            "clientName"=>"",
+            "filter"=>$filter,
+        ];
+        $response = Http::withBasicAuth('joe','secret')->post('http://localhost:10010/trx/getTrx', $payload)->json();
+        // dd($response);
+        if (!is_array($response) || !isset($response['result']) || !is_array($response['result'])) {
+            return response()->json(['error' => 'Invalid API response format or data type'], 500);
+        }
+        $response = $response['result'];
+        return $response;    
+    }
+
+
+}
